ff87f3487b6b4a10b4aa6efa71dcbf39a59775f3
luginbuehln <luginbuehln@bzz.ch>
2021-01-20 15:51:28 +0100

    projectDao

--- src/main/java/ch/bzz/refproject/data/ProjectDao.java
+++ src/main/java/ch/bzz/refproject/data/ProjectDao.java
@@ -1,10 +1,143 @@
 package ch.bzz.refproject.data;
 
+import ch.bzz.refproject.model.Category;
 import ch.bzz.refproject.model.Project;
+import ch.bzz.refproject.util.Result;
+
+import javax.print.DocFlavor;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 public class ProjectDao implements Dao<Project, String>{
     /**
      * default constructor
      */
     public ProjectDao() {}
+
+    /**
+     * list all project in the table "RefProject"
+     *
+     * @return list of projects
+     */
+
+    @Override
+    public List<Project> getAll() {
+        List<Project> projectList = new ArrayList<>();
+        String sqlQuery="SELECT * FROM RefProject.Project;";
+
+        try {
+            ResultSet resultSet = MySqlDB.sqlSelect(sqlQuery);
+            while (resultSet.next()){
+                Project project = new Project();
+                setValues(resultSet, project);
+                projectList.add(project);
+            }
+        } catch (SQLException sqlEx){
+            MySqlDB.printSQLException(sqlEx);
+            throw new RuntimeException();
+        } finally {
+
+            MySqlDB.sqlClose();
+        }
+        return projectList;
+    }
+
+
+    /**
+     * read a single project from the table RefProject
+     *
+     * @param projectUUID primary kex
+     * @return project object
+     */
+
+    @Override
+    public Project getEntity(String projectUUID) {
+        Project project = new Project;
+
+        String sqlQuery = "";
+
+        Map<Integer, String> values = new HashMap<>();
+        values.put(1,projectUUID);
+        try {
+            ResultSet resultSet = MySqlDB.sqlSelect(sqlQuery);
+            if (resultSet.next()){
+                setValues(resultSet, project);
+            }
+        } catch (SQLException sqlEx){
+            sqlEx.printStackTrace();
+            throw new RuntimeException();
+        } finally {
+            MySqlDB.sqlClose();
+        }
+        return project;
+    }
+
+    /**
+     * deletes a project in the table RefProject
+     *
+     * @param projectUUID primary kex
+     * @return Result
+     */
+
+    @Override
+    public Result delete(String projectUUID) {
+        String sqlQuery= "";
+        Map<Integer, String> values= new HashMap<>();
+        values.put(1, projectUUID);
+        try {
+            return MySqlDB.sqlUpdate(sqlQuery, values);
+
+        } catch (SQLException sqlEx){
+            throw new RuntimeException();
+        }
+    }
+
+    /**
+     * saves a new project in the table RefProject
+     *
+     * @param project
+     * @return Result
+     */
+
+    @Override
+    public Result save(Project project) {
+        Map<Integer, String> values = new HashMap<>();
+        String sqlQuery;
+        if (project.getProjectUUID() == null){
+            project.setProjectUUID(UUID.randomUUID().toString());
+            sqlQuery="INSERT INTO Project";
+        } else {
+            sqlQuery="REPLACE Project";
+        }
+        sqlQuery += "SET projectUUID=?, "+
+                " categoryUUID=?, " +
+                " title=?, " +
+                " status=?, " +
+                " startDate=?, " +
+                " endDate=?";
+
+        values.put(1, project.getProjectUUID());
+        values.put(2, project.getCategory().getCategoryUUID());
+        values.put(3, project.getTitle());
+        values.put(4, project.getStatus());
+        values.put(5, String.valueOf(project.getStartDate()));
+        values.put(5, String.valueOf(project.getEndDate()));
+
+        try {
+            return MySqlDB.sqlUpdate(sqlQuery, values);
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    private void setValues(ResultSet resultSet, Project project) throws SQLException{
+        project.setProjectUUID(resultSet.getString("projectUUID"));
+        project.setCategory(new Category());
+        project.getCategory().setCategoryUUID(resultSet.getString("categoryUUID"));
+        project.getCategory().setTitle(resultSet.getString("title"));
+        project.setTitle(resultSet.getString("title"));
+        project.setStatus(resultSet.getString("status"));
+    }
 }
